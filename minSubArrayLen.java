7c365a34db28d03253a406d9277610c4893843f8
luv8436 <luvkumar8436@gmail.com>
2022-01-05 03:02:01 +0530

    subarray

---
+++ minSubArrayLen.java
@@ -0,0 +1,23 @@
+public class minSubArrayLen {
+    public int minSubArrayLen(int target, int[] nums) {
+        int sum=0;
+        int si=0;
+        int minLength=Integer.MAX_VALUE;
+        for(int index=0;index<nums.length;index++){
+            sum+=nums[index];
+            while(si<=index&&sum>=target){
+                int length = index-si+1;
+                if(length<minLength){
+                    minLength=length;
+                }
+                sum=sum-nums[si];
+                si++;
+            }
+        }
+        if(minLength!=Integer.MAX_VALUE){
+            return minLength;
+        }
+        
+        return 0;
+    }
+}
