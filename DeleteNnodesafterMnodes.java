91f6d1cbae526200d2c6992138a93f9736e35495
luv8436 <luvkumar8436@gmail.com>
2022-01-11 23:14:49 +0530

    delete N nodes after skipping M nodes

---
+++ DeleteNnodesafterMnodes.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class DeleteNnodesafterMnodes {
+    class Node {
+    	int data;
+    	Node left;
+    	Node right;
+    
+    	Node(int data) {
+    		this.data = data;
+    		left = null;
+    		right = null;
+    	}
+    }
+    static void linkdelete(Node head, int M, int N)
+    {
+        // your code here
+        Node temp = head;
+        int count=1;
+        while(temp!=null&&count<M ){
+            temp=temp.next;
+            count++;
+        }
+        if(temp==null||temp.next==null){
+            return ;
+        }
+        
+        Node ptr = temp.next;
+        count=1;
+        while(ptr!=null&&count<=N){
+            ptr=ptr.next;
+            count++;
+        }
+        temp.next = ptr;
+        linkdelete(ptr , M , N);
+    }
+}
